0fdf30cc317b73d4089c814397b561db41ec2d45
mxxt1 <matt@dustcloudlabs.com>
2020-02-17 15:25:12 -0500

    bubble sort working, tests passing

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -11,7 +11,7 @@ def selection_sort( arr ):
         for j in range(i+1,len(arr)):
             if arr[j] < arr[smallest]:
                 smallest = j
-                           
+
         arr[i],arr[smallest] = arr[smallest],arr[i]
         # arr[j] = arr[i]
         # arr[i] = temp
@@ -21,7 +21,13 @@ def selection_sort( arr ):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    for i in range(0,len(arr)-1):
+        for j in range(i+1, len(arr)):
+            if arr[i] > arr[j]:
+                # print('lower')
+                print(arr)
+                arr[i], arr[j] = arr[j], arr[i]
+    
     return arr
 
 
