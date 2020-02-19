89ba4a9b537de34ff69b780a0c9ac85003e53769
mxxt1 <matt@dustcloudlabs.com>
2020-02-19 14:12:29 -0500

    merge and merge_sort working, MVP complete

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -4,12 +4,31 @@ def merge( arrA, arrB ):
     elements = len( arrA ) + len( arrB )
     # create list with length of elements
     merged_arr = [0] * elements
-    # TO-DO
-    merged_arr = arrA + arrB
-    # print(merged_arr)
+    # # TO-DO
+    # merged_arr = arrA + arrB
+    # # print(merged_arr)
     
-    merged_arr.sort()
+    # merged_arr.sort()
     # print(merged_arr)
+    i = j = 0
+    sorted_index = 0
+
+    while i<len(arrA) and j<len(arrB):
+        if arrA[i] > arrB[j]:
+            merged_arr[sorted_index] = arrB[j]
+            j += 1
+            sorted_index += 1
+        elif arrA[i] < arrB[j]:
+            merged_arr[sorted_index] = arrA[i]
+            i += 1
+            sorted_index += 1
+
+    merged_arr = merged_arr[:sorted_index]
+    if i < len(arrA):
+        merged_arr = merged_arr + arrA[i:]
+    if j < len(arrB):
+        merged_arr = merged_arr + arrB[j:]
+
     return merged_arr
 
 merge([1,3,5,7,9], [0,2,4,6,8])
@@ -17,8 +36,20 @@ merge([1,3,5,7,9], [0,2,4,6,8])
 # TO-DO: implement the Merge Sort function below USING RECURSION
 def merge_sort( arr ):
     # TO-DO
+    if (len(arr)<=1):
+        return arr
+    
+    pivot = int(len(arr)/2)
+    left = arr[:pivot]
+    right = arr[pivot:]
 
-    return arr
+    # sort left and right
+    left_sort = merge_sort(left)
+    right_sort = merge_sort(right)
+
+
+    
+    return merge(left_sort,right_sort)
 
 
 # STRETCH: implement an in-place merge sort algorithm
